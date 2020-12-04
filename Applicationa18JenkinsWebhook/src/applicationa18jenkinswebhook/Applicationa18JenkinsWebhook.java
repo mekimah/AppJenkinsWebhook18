87a58576e03b1ddc856f723a1aabe2acf883467b
mekimah <tmekimah@github.com>
2020-12-04 13:23:51 -0500

    [webhook] webhook

---
+++ Applicationa18JenkinsWebhook/src/applicationa18jenkinswebhook/Applicationa18JenkinsWebhook.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package applicationa18jenkinswebhook;
+
+/**
+ *
+ * @author HP
+ */
+public class Applicationa18JenkinsWebhook {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Bonjour "+args[1]+", " +args[0]);
+
+    }
+    
+}
